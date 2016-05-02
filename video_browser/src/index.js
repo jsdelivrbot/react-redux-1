4ea489093124686709b6e526154335b3bcf85810
Dave Boyle <dboyle@criticalmass.com>
2016-05-02 16:52:43 -0400

    Updated search_bar.js

--- video_browser/src/index.js
+++ video_browser/src/index.js
@@ -8,8 +8,15 @@ const API_KEY = 'AIzaSyBKl9ovXZDzEa7b1CNYXrZ2ZAQ-jxw5WOY';
 
 //=> is used instead of function
 const App = () => {
-	return <SearchBar />;
-
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 ReactDom.render(<App />, document.querySelector('.container'));
+
+//2 types of components
+// 1) Functional Component - The component is literally a function, some info goes in, JSX comes out
+// 2) Class Based Component - Used when we want some type of internal record keeping; state
