9956f0c555a53309d3a864681218d0e78755f06a
Dave Boyle <dboyle@criticalmass.com>
2016-06-09 16:40:24 -0400

    Added CommentBox component.

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -1,14 +1,19 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '..//test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// Use 'descrive' to group together similiar tests
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+	let component;
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+	beforeEach(() => {
+		component = renderComponent(App);
+	});
+
+	// Use 'it' to test a single attribute of a target
+	it('it shows a comment box', () => {
+		expect(component.find('.comment-box')).to.exist;
+	});
+}) ;
+
+//We want to test the end product not the internal methods of the app
