b8e0ff4a1703fb39130ffa19dae97c24fe9db797
David Boyle <spektraldevelopment@Davids-MacBook-Pro.local>
2018-10-04 19:45:47 -0400

    [refresher] - Section 1.13

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const App = function() {
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const API_KEY = 'AIzaSyAMhZkB8XdbsXN-aVI-0BjLtyUkwntpLvw';
+
+const App = () => {
     return <div>Hi!</div>;
 }
+
+ReactDOM.render(<App />, document.querySelector(".container"));
